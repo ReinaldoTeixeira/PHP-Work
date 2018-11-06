45b1d4ae8470e5af405d7ca960765ad341342c6a
raina.pepe <raina.pepe@jbtec.com.br>
2018-11-06 21:26:18 -0200

    dockerfile add

--- model/conexao.php
+++ model/conexao.php
@@ -1,10 +1,10 @@
 <?php
 	try {
-	  $con = new PDO("mysql:host=127.0.0.1;port=8889;dbname=fib", "admin", "admin");
+		echo "TESTE ----------------------------------------------------------------------------\n\n";
+	  	$con = new PDO("mysql:host=localhost;dbname=fib", "admin", "admin");
 	    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	    $con->exec("SET CHARACTER SET utf8");
 	} 
-
 	catch(PDOException $e) {
 	    echo 'ERROR: ' . $e->getMessage();
 	}
