507e5aff8e31b8d70922e223760c12a95ebb56ef
Reinaldo Teixeira <reinaldo.teixeira@outlook.com>
2018-11-06 21:28:03 -0200

    merge

--- model/conexao.php
+++ model/conexao.php
@@ -1,6 +1,6 @@
 <?php
 	try {
-	  $con = new PDO("mysql:host=127.0.0.1;port=8889;dbname=fib", "admin", "admin");
+	  $con = new PDO("mysql:host=127.0.0.1;port=3307;dbname=fib", "admin", "admin");
 	    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	    $con->exec("SET CHARACTER SET utf8");
 	} 
